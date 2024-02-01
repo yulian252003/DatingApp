baffabd84e57f36e6dfb4c95fcee186f1626ef2d
julian <yulian252003@hotmail.com>
2024-02-01 10:49:57 -0500

    toaster,routes and other components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,16 +7,24 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
 import { FormsModule } from '@angular/forms';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { MembersListComponent } from './members/members-list/members-list.component';
+import { MembersDetailComponent } from './members/members-detail/members-detail.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { SharedModule } from './_modules/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MembersListComponent,
+    MembersDetailComponent,
+    ListsComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
@@ -24,7 +32,7 @@ import { RegisterComponent } from './register/register.component';
     HttpClientModule,
     BrowserAnimationsModule, //Ese se puso a mano. Los otros estaban
     FormsModule, 
-    BsDropdownModule.forRoot()// Se incluyo a mano.
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -11,7 +12,7 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();//From child to parent
   model: any = {}
 
-constructor(private accountService: AccountService){ }
+constructor(private accountService: AccountService, private toastr: ToastrService){ }
 
 ngOnInit(): void {}
 
@@ -21,7 +22,10 @@ register(){
     //console.log(response);
     this.cancel();
     },
-    error: error =>console.log(error)
+    error: error =>{
+      this.toastr.error(error.error),
+      console.log(error)
+    } 
   })
 }
 cancel(){
