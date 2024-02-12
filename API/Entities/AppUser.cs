4380c897aafdcacb183faaa2bb1fe065ce0fd876
julian <yulian252003@hotmail.com>
2024-02-12 11:31:34 -0500

    mappint and repository implementation

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,3 +1,7 @@
+using API.Extensions;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.SignalR.Protocol;
+
 namespace API.Entities;
 
 public class AppUser
@@ -8,4 +12,44 @@ public class AppUser
     public byte[] PasswordHash {get; set;}
 
     public byte[] PasswordSalt {get; set;}
+
+    public DateOnly DateOfBirth{get; set;}
+
+    public string KnownAs {get; set;}
+
+    public DateTime Created {get; set;} = DateTime.UtcNow;
+
+    public DateTime LastActive {get; set;} = DateTime.UtcNow;
+
+    public string Gender{get; set;}
+
+    public string Introduction{get; set;}
+
+    public string LookingFor {get; set;}
+
+    public string Interests {get; set;}
+
+    public string City {get; set;}
+
+    public string Country {get; set;}
+
+    public List<Photo> Photos {get; set;} = new ();
+
+/*
+    public int GetAge()
+    {
+        return DateOfBirth.CalculateAge();
+    }
+
+ This is an alternative to the code implemente in line 12 class: AutoMapperProfiles
+     public string GetPhotoUrl()
+      {
+            foreach (var photo in Photos)
+            {
+                if (photo.IsMain)
+                    return photo.Url;
+            }
+            return null; // or return a default photo URL or an empty string
+        }
+        */
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,8 @@
 
+using API.Data;
 using API.Extensions;
 using API.Middleware;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,5 +26,18 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch(Exception ex)
+{
+    var logger = services.GetService<ILogger<Program>>();
+    logger.LogError(ex, "An error ocurred during migration");
+}
 
 app.Run();
